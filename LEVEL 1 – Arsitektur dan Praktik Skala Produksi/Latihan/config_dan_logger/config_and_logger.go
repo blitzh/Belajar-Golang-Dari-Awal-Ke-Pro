6ace9bb97379d97d9131e33726eb4750235ad71a
therif <th3rif@gmail.com>
2025-05-30 16:29:29 +0700

    update config

--- LEVEL 1 – Arsitektur dan Praktik Skala Produksi/Latihan/config_dan_logger/config_and_logger.go
+++ LEVEL 1 – Arsitektur dan Praktik Skala Produksi/Latihan/config_dan_logger/config_and_logger.go
@@ -1,22 +1,37 @@
-
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-    logFile, err := os.Create("app.log")
-    if err != nil {
-        log.Fatal("failed to create log file")
-    }
-    defer logFile.Close()
 
-    log.SetOutput(logFile)
-    log.Println("App started")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-    configValue := os.Getenv("APP_ENV")
-    fmt.Println("Config:", configValue)
+	logFile, err := os.Create("app.log")
+	if err != nil {
+		log.Fatal("failed to create log file")
+	}
+	defer logFile.Close()
+
+	log.SetOutput(logFile)
+	log.Println("App started")
+
+	// load environment variable form file and set to file
+
+	// retrieve and print the environment variable
+	// Akses variabel environment
+	appEnv := os.Getenv("APP_ENV")
+	fmt.Println("APP_ENV:", appEnv)
+
+	debug := os.Getenv("APP_DEBUG")
+	fmt.Println("DEBUG:", debug)
+
 }
